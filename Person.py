c959710412ad69c949e45399fb76dfb3c0d11367
breakfastset <kleng99@gmail.com>
2020-08-14 18:09:22 +0800

    Add most valuable player

--- Person.py
+++ Person.py
@@ -3,5 +3,9 @@ class Person:
         self.name = "Jordan"
         self.number = 23
 
+    def is_most_valuable_player(self):
+        return True
+
+
     def __str__(self):
         return "{} - {}".format(self.name, self.number)
